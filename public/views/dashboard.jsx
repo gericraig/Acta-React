4e8d4af6aa7ff19b4afa392c431cd87e4ee52bf2
Geralyn Craig <gericraig18@gmail.com>
2019-02-23 09:30:02 -0800

    Revert "fix listing"
    This reverts commit 35d717d1a69f2ceba825a16b2840dfb1011fdd3a.

--- public/views/dashboard.jsx
+++ public/views/dashboard.jsx
@@ -1,11 +1,6 @@
 
 var Layout = require('./layout.jsx');
 var React = require('react');
-var ReactDOM = require('react-dom');
-
-const updatedList = this.props.entrees.map((content)=>{
-    return <li>{content}</li>;
-});
 
 module.exports = React.createClass({
 
@@ -16,7 +11,7 @@ module.exports = React.createClass({
                 <h1>Hi, Welcome to your Dashboard</h1>
                 <h2>Here are your recent entries</h2>
                 <ul>
-                    {updatedList}
+                    <li>blah</li>
                 </ul>
             </Layout>
         );
